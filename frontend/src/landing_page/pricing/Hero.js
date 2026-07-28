418381b2fcabaa31abb2162e92d7880a279c066b
Taha cutlery <tahacutlery@gmail.com>
2026-07-29 00:12:00 +0530

    Pricing Component

--- frontend/src/landing_page/pricing/Hero.js
+++ frontend/src/landing_page/pricing/Hero.js
@@ -2,7 +2,25 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="container mt-5 p-5">
+      <div className="row p-5 text-center">
+        <div className="col-4">
+          <img src="media/images/pricing0.svg" className="w-75" />
+          <h2>Free equity delivery</h2>
+          <p className="text-muted">All equity delivery investments (NSE, BSE), are absolutely free — ₹ 0 brokerage.</p>
+        </div>
+        <div className="col-4">
+          <img src="media/images/other-trades.svg" className="w-75" />
+          <h2>Intraday and F&O trades</h2>
+          <p className="text-muted">Flat ₹ 20 or 0.03% (whichever is lower) per executed order on intraday trades across equity, currency, and commodity trades. Flat ₹20 on all option trades.</p>
+        </div>
+        <div className="col-4">
+          <img src="media/images/pricing0.svg" className="w-75" />
+          <h2>Free direct MF</h2>
+          <p className="text-muted">All direct mutual fund investments are absolutely free — ₹ 0 commissions & DP charges.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
